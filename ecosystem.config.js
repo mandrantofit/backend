39157d70e7e8c7062c670d487fbafb9f8f941949
mandrantofit <mandrantofit@gmail.com>
2024-09-19 11:28:18 +0300

    Add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'dev-stock-backend',    // Nom de l'application
       script: './index.js',         // Chemin vers votre fichier principal
-      instances: 'max',             // Utiliser tous les cœurs du CPU disponibles
+      instances: 1,             // Utiliser tous les cœurs du CPU disponibles
       exec_mode: 'cluster',         // Mode cluster pour améliorer la performance
       watch: true,                  // Recharger automatiquement en cas de modifications des fichiers
       env: {
