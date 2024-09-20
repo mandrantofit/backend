56e1da72ca8411acf825c66f32493c65a5949a8d
mandrantofit <mandrantofit@gmail.com>
2024-09-20 13:55:33 +0300

    Test Docker build n Push

--- model/database.js
+++ model/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: '127.0.0.1', //172.25.52.205
-  user: 'root', //cpadmin
-  password: '', //adminplus
+  user: 'mandrantofit', //cpadmin
+  password: 'abcd1234', //adminplus
   database: 'stock', //stock
 });
 
